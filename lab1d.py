493defc13c7188aac45233c77dbc2ba145efad14
vpatel255 <vilaspatel736736@gmail.com>
2024-06-01 06:51:29 -0400

    added lab1d

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Define the integer objects
+x = 10
+y = 2
+z = 5
+
+# Calculate the expression
+result = x + y * z
+
+# Print the result
+print(f"{x} + {y} * {z} = {result}")
+
