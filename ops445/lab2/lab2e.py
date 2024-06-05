554206170b4906918c211eb27278b51d906a9377
vpatel255 <vpatel255@myseneca.ca>
2024-06-05 03:12:42 -0400

    added lab2e.py

---
+++ ops445/lab2/lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: [Vilaskumar Patel]
+# Author ID: [157545211]
+# Date Created: 2024/06/05
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print('blast off!')
+
