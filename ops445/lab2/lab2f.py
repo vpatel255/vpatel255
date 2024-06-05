36bae0821d5a16de9794a51e63448f793b2bc534
vpatel255 <vpatel255@myseneca.ca>
2024-06-05 03:21:56 -0400

    added lab2f.py

--- ops445/lab2/lab2f.py
+++ ops445/lab2/lab2f.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-# Author: [Vilaskumar Patel]
-# Author ID: [157545211]
-# Date Created: 2024/06/05
+# Author: [Your Full Name]
+# Author ID: [Your Seneca ID]
+# Date Created: yyyy/mm/dd
 
-timer = 3
+import sys
 
+# Convert the first command-line argument to an integer
+timer = int(sys.argv[1])
+
+# While loop to count down from timer to 1
 while timer > 0:
     print(timer)
     timer -= 1
 
+# Print "blast off!" when the countdown is complete
 print('blast off!')
 
