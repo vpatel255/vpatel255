569f1fa55b5ec8e0df9b91f93e665549a9239e66
vpatel255 <vpatel255@myseneca.ca>
2024-06-05 09:59:00 -0400

    added lab3a.py

--- ops445/lab3/lab3a.py
+++ ops445/lab3/lab3a.py
@@ -1,1 +1,24 @@
-hii
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: [157545211]
+
+def return_text_value():
+    return "Good Morning Terry"
+
+# return_number_value() function
+
+def return_number_value():
+    return 15
+
+# Main Program
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
+
