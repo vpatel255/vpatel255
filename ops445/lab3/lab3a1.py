f704ba36e886cb7c42241b2e8f1e592dc8106f34
vpatel255 <vpatel255@myseneca.ca>
2024-06-05 09:59:47 -0400

    added lab3a1.py

---
+++ ops445/lab3/lab3a1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: [seneca_id]
+
+def return_text_value():
+    return "Hello from return_text_value function!"
+
+# return_number_value() function
+
+def return_number_value():
+    return 42
+
+# Main Program
+
+import lab3a
+text = lab3a.return_text_value()
+print(text)
+print(lab3a.return_number_value())
