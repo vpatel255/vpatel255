3c9b687bf1b3df5c2bab3c897e2f082ae24b41cc
vpatel255 <vpatel255@myseneca.ca>
2024-07-21 02:24:01 -0400

    added lab7i.py

---
+++ ops445/lab7/lab7f.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# Student ID: vpatel255
+
+class Time:
+    """Simple object type for time of the day.
+       data attributes: hour, minute, second
+       function attributes: __init__, __str__, __repr__,
+                            time_to_sec, format_time,
+                            change_time, sum_times, __add__
+    """
+    def __init__(self, hour=12, minute=0, second=0):
+        """Constructor for time object"""
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def __str__(self):
+        """Return a string representation for the object self"""
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        """Return a string representation for the object self"""
+        """Use '.' instead of ':' in the formatting string."""
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
+    def format_time(self):
+        """Return time object (t) as a formatted string"""
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def sum_times(self, t2):
+        """Add two time objects and return the sum."""
+        total_seconds = self.time_to_sec() + t2.time_to_sec()
+        return sec_to_time(total_seconds)
+
+    def change_time(self, seconds):
+        """Change the time object by a number of seconds."""
+        time_seconds = self.time_to_sec()
+        nt = sec_to_time(time_seconds + seconds)
+        self.hour, self.minute, self.second = nt.hour, nt.minute, nt.second
+        return None
+
+    def time_to_sec(self):
+        """Convert a time object to a single integer representing the number of seconds from midnight."""
+        minutes = self.hour * 60 + self.minute
+        seconds = minutes * 60 + self.second
+        return seconds
+
+    def valid_time(self):
+        """Check for the validity of the time object attributes:
+           0 <= hour < 24, 0 <= minute < 60, 0 <= second < 60"""
+        if self.hour < 0 or self.minute < 0 or self.second < 0:
+            return False
+        if self.minute >= 60 or self.second >= 60 or self.hour >= 24:
+            return False
+        return True
+
+    def __add__(self, t2):
+        """Return the result by using sum_times() method"""
+        return self.sum_times(t2)
+
+def sec_to_time(seconds):
+    """Convert a given number of seconds to a time object in hour, minute, second format."""
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
+
---
+++ ops445/lab7/lab7h.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Student ID: vpatel255
+
+def function1():
+    print('print() in function1 on schoolName:', schoolName)
+
+def function2():
+    print('print() in function2 on schoolName:', schoolName)
+
+# Global variable
+schoolName = 'Seneca College'
+
+print('print() in main on schoolName:', schoolName)
+function1()
+print('print() in main on schoolName:', schoolName)
+function2()
+print('print() in main on schoolName:', schoolName)
+
